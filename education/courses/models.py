9b2a5d5fa21258fa5e247ae62e6462fc7fd318ef
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-03-31 18:33:31 +0000

    auth templates and urls

--- education/courses/models.py
+++ education/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
+from .fields import OrderField
 
 
 # Create your models here.
@@ -37,8 +38,13 @@ class Module(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
+    order = OrderField(blank=True, for_fields=['course'])
+
+    class Meta:
+        ordering = ['order']
+
     def __str__(self):
-        return self.title
+        return f'{self.order}. {self.title}'
 
 
 class Content(models.Model):
@@ -51,6 +57,10 @@ class Content(models.Model):
     )})
     object_id = models.PositiveIntegerField()
     item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['module'])
+
+    class Meta:
+        ordering = ['order']
 
 
 class ItemBase(models.Model):
