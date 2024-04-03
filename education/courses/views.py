941cbe1d8e5a527401a83a4e107c1ad18cb65cd4
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2024-04-03 06:03:32 +0000

    student enroll form and template added

--- education/courses/views.py
+++ education/courses/views.py
@@ -11,6 +11,8 @@ from .models import Course, Module, Content, Subject
 from .forms import ModelFormSet
 from django.db.models import Count
 
+from students.forms import CourseEnrollForm
+
 
 # Create your views here.
 class ContentCreateUpdateView(TemplateResponseMixin, View):
@@ -158,3 +160,10 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/course_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        return context
+
